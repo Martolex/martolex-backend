e7823512dba5702db4cdfccf0dc213d833ffed8e
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-10-07 16:31:52 +0530

    cart stats in admin

--- routes/AdminRoutes/Users.js
+++ routes/AdminRoutes/Users.js
@@ -1,5 +1,5 @@
-const { User } = require("../../models");
-
+const { User, Cart, Book, BookRent } = require("../../models");
+const Sequelize = require("sequelize");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
@@ -16,6 +16,50 @@ router.get("/", async (req, res) => {
     res.json({ code: 1, data: users });
   } catch (err) {
     res.json({ code: 0, message: "Something went wrong" });
+  }
+});
+
+router.get("/:id/cart", async (req, res) => {
+  try {
+    const userCart = await Cart.findAll({
+      userId: req.params.id,
+      attributes: ["plan", "id", "qty"],
+      order: [["createdAt", "DESC"]],
+      include: {
+        model: Book,
+        attributes: ["name", "author", "id", "publisher", "isbn", "edition"],
+        as: "book",
+        include: {
+          model: User,
+          as: "upload",
+          attributes: ["id", "name", "isAdmin"],
+        },
+      },
+    });
+
+    res.json({ code: 1, data: userCart });
+  } catch (err) {
+    throw err;
+    res.json({ code: 0, message: "Something went wrong" });
+  }
+});
+
+router.get("/cartStats", async (req, res) => {
+  try {
+    const userCarts = await User.findAll({
+      attributes: [
+        "id",
+        "name",
+        "email",
+        "phoneNo",
+        [Sequelize.fn("count", Sequelize.col("cartItems.id")), "itemCount"],
+      ],
+      include: { model: Cart, as: "cartItems", required: true, attributes: [] },
+    });
+    res.json({ code: 1, data: userCarts });
+  } catch (err) {
+    throw err;
+    res.json({ code: 0, message: "something went wrong" });
   }
 });
 
