7708589e9266f64f97912f646bfa5ab9d64ded2a
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-10-16 14:52:54 +0530

    referral code orders

--- routes/studentAmbassador/orders.js
+++ routes/studentAmbassador/orders.js
@@ -1,9 +1,10 @@
 const sequelize = require("../../config/db");
 const { commisionRate } = require("./config");
-
+const { Order, OrderItem, User } = require("../../models");
+const { Sequelize, Op } = require("sequelize");
 const router = require("express").Router();
 
-router.route("/").get(async (req, res) => {
+router.route("/leads").get(async (req, res) => {
   try {
     const query = `select orderId,email ,name,phoneNo, 
     format(sum(rent+deposit),2) as orderTotal ,sum(qty) as booksCount, orderDate,
@@ -22,6 +23,48 @@ router.route("/").get(async (req, res) => {
   } catch (err) {
     console.log(err);
     res.json({ code: 0, message: "something went wrong" });
+  }
+});
+
+router.route("/couponCode").get(async (req, res) => {
+  try {
+    const orderTotalQuery = Sequelize.fn(
+      "sum",
+      Sequelize.where(Sequelize.col("rent"), "+", Sequelize.col("deposit"))
+    );
+    const orders = await Order.findAll({
+      attributes: [
+        ["id", "orderId"],
+        [Sequelize.col("user.name"), "name"],
+        [Sequelize.col("user.email"), "email"],
+        [Sequelize.col("user.phoneNo"), "phoneNo"],
+        ["createdAt", "orderDate"],
+
+        [orderTotalQuery, "orderTotal"],
+        [Sequelize.fn("sum", Sequelize.col("items.qty")), "booksCount"],
+      ],
+      order: [["createdAt", "DESC"]],
+
+      group: ["items.orderId"],
+      where: {
+        referralCode: {
+          [Op.eq]: Sequelize.literal(
+            `(select referralCode from AmbassadorDetails where id='${req.user.ambassadorId}')`
+          ),
+        },
+      },
+      include: [
+        { model: OrderItem, as: "items", attributes: [] },
+        { model: User, as: "user", attributes: [] },
+      ],
+    });
+    const data = orders.map((order) => ({
+      ...order.toJSON(),
+      earning: commisionRate * order.toJSON().orderTotal.toFixed(2),
+    }));
+    res.json({ code: 1, data });
+  } catch (err) {
+    console.log(err);
   }
 });
 module.exports = router;
