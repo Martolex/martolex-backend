d6c82105a8d9be14ddacbf9d760e36b55e5fbcfe
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-12-06 13:20:38 +0530

    config from environment

--- src/utils/Payments/config.js
+++ src/utils/Payments/config.js
@@ -1,8 +1,10 @@
 const config = require("../../config/config");
 
-module.exports.API_ID = "44732aab419a889d25f76bc7f23744";
+module.exports.API_ID =
+  process.env.CASHFREE_API_ID || "44732aab419a889d25f76bc7f23744";
 
-module.exports.SECRET_KEY = "cddc57061079ce23527601e52b2b8b8df86cdc11";
+module.exports.SECRET_KEY =
+  process.env.CASHFREE_SECRET || "cddc57061079ce23527601e52b2b8b8df86cdc11";
 
 const CASHFREE_PROD_API = "https://api.cashfree.com";
 const CASHFREE_TEST_API = "https://test.cashfree.com";
