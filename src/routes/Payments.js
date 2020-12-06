95723f577f9ec2feaf84f47767a2acf2ab8c14cf
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-12-06 13:00:03 +0530

    config fixes

--- src/routes/Payments.js
+++ src/routes/Payments.js
@@ -3,33 +3,38 @@ const { config } = require("../config/config");
 const { Order } = require("../models");
 const querystring = require("querystring");
 const { paymentStatus } = require("../utils/enums");
+const verifySignature = require("../utils/Payments/verifySignature");
 router.route("/verify").post(async (req, res) => {
   const paymentDetails = req.body;
   const USER_APP = `${config.applications.USER_APP}/order`;
-  if (paymentDetails.txStatus == "SUCCESS") {
-    Order.update(
-      {
-        paymentStatus: paymentStatus.PAID,
-        gatewayRefId: paymentDetails.referenceId,
-        gateWayMode: paymentDetails.paymentMode,
-      },
-      { where: { gatewayOrderId: paymentDetails.orderId } }
-    );
+  if (verifySignature(paymentDetails)) {
+    if (paymentDetails.txStatus == "SUCCESS") {
+      Order.update(
+        {
+          paymentStatus: paymentStatus.PAID,
+          gatewayRefId: paymentDetails.referenceId,
+          gateWayMode: paymentDetails.paymentMode,
+        },
+        { where: { gatewayOrderId: paymentDetails.orderId } }
+      );
 
-    const orders = await Order.findAll({
-      where: { gatewayOrderId: paymentDetails.orderId },
-      attributes: ["id"],
-    });
-    const orderIds = orders.map((order) => order.id);
-    const orderIdsQueryString = querystring.stringify({
-      orders: orderIds,
-    });
-    redirectURL = `${USER_APP}/confirmation?${orderIdsQueryString}`;
+      const orders = await Order.findAll({
+        where: { gatewayOrderId: paymentDetails.orderId },
+        attributes: ["id"],
+      });
+      const orderIds = orders.map((order) => order.id);
+      const orderIdsQueryString = querystring.stringify({
+        orders: orderIds,
+      });
+      redirectURL = `${USER_APP}/confirmation?${orderIdsQueryString}`;
+      res.status(302).redirect(redirectURL);
+    } else {
+      redirectURL = `${USER_APP}/failure`;
+    }
     res.status(302).redirect(redirectURL);
   } else {
-    redirectURL = `${USER_APP}/failure`;
+    res.status(400).send({ code: 0, message: "unauthorized" });
   }
-  res.status(302).redirect(redirectURL);
 });
 
 module.exports = router;
--- src/utils/Payments/payments.js
+++ src/utils/Payments/payments.js
@@ -1,6 +1,7 @@
 const getPaymentLink = require("./getPaymentLink");
 const { v4: UUIDV4 } = require("uuid");
-const test = async () => {
+const verifySignature = require("./verifySignature");
+const testPaymentLink = async () => {
   try {
     const res = await getPaymentLink(UUIDV4(), {
       orderIds: [UUIDV4(), UUIDV4()],
@@ -15,4 +16,19 @@ const test = async () => {
   }
 };
 
-test();
+const testSignatureVerification = () => {
+  console.log(
+    verifySignature({
+      orderId: "1343a4a1-cbf5-47c9-bbb7-7ca949ca371c",
+      orderAmount: "200.00",
+      referenceId: "640302",
+      txStatus: "SUCCESS",
+      paymentMode: "CREDIT_CARD",
+      txMsg: "Transaction Successful",
+      txTime: "2020-12-05 23:22:02",
+      signature: "l7ALfoCT+oRfWMb32EP3KSlSTLcHdBJtFYLdvYkQ5u8=",
+    })
+  );
+};
+testSignatureVerification();
+// testPaymentLink();
