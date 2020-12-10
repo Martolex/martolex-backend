9c0ba09507f7f597e0e8550955ee9a206be7929e
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-12-10 13:44:31 +0530

    business logic prevent status transition if not paid

--- src/utils/orderUtils.js
+++ src/utils/orderUtils.js
@@ -1,4 +1,10 @@
-const { plans, planDuration, orderStatus } = require("./enums");
+const {
+  plans,
+  planDuration,
+  orderStatus,
+  paymentModes,
+  paymentStatus,
+} = require("./enums");
 
 const returnPeriod = 7 * 24 * 60 * 60 * 1000;
 const getReturnDate = (plan) => {
@@ -31,16 +37,28 @@ const OrderTotal = (items) => {
   return items.reduce((total, item) => total + item.qty * item.deposit, 0);
 };
 
-const validateStatus = (oldStatus, newStatus) => {
-  console.log(oldStatus, newStatus);
+const validateStatus = (order, newStatus) => {
+  if (
+    order.paymentMode === paymentModes.CASHFREE &&
+    order.paymentStatus !== paymentStatus.PAID
+  )
+    return false;
   if (!Object.values(orderStatus).includes(newStatus)) return false;
   const precedence = {
     [orderStatus.PROCESSING]: 1,
     [orderStatus.SHIPPED]: 2,
     [orderStatus.DELIVERED]: 3,
   };
-  if (precedence[newStatus] !== precedence[oldStatus] + 1) return false;
+  if (precedence[newStatus] !== precedence[order.orderStatus] + 1) return false;
   return true;
 };
 
-module.exports = { getReturnDate, OrderTotal, itemPrice, validateStatus };
+const createOrderFilters = (filters) => {};
+
+module.exports = {
+  getReturnDate,
+  OrderTotal,
+  itemPrice,
+  validateStatus,
+  createOrderFilters,
+};
