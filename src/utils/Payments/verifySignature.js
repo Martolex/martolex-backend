9b332d3602dfd04f8e86898bd483f2398221fcec
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-12-12 16:46:04 +0530

    verify request signature fixed

--- src/utils/Payments/verifySignature.js
+++ src/utils/Payments/verifySignature.js
@@ -1,25 +1,19 @@
 const crypto = require("crypto-js");
 const config = require("./config");
 
-const base64Encode = (hash) => {
-  const encodedWord = crypto.enc.Utf8.parse(hash); // encodedWord Array object
-  const encoded = crypto.enc.Base64.stringify(encodedWord);
-  return encoded;
-};
 module.exports = (payload = {}) => {
   const { signature, ...data } = payload;
 
-  const orderedData = {};
-  Object.keys(data)
-    .sort()
-    .forEach(function (key) {
-      orderedData[key] = data[key];
-    });
-  const message = Object.values(orderedData).reduce(
+  const message = Object.values(data).reduce(
     (prev, curr) => (prev += curr),
     ""
   );
-  const hash = crypto.SHA256(message, config.SECRET_KEY).toString();
-  const base64 = base64Encode(hash);
-  return true;
+
+  const hash = crypto.HmacSHA256(
+    crypto.enc.Utf8.parse(message),
+    config.SECRET_KEY
+  );
+  const base64Hash = crypto.enc.Base64.stringify(hash);
+  [];
+  return signature == base64Hash;
 };
