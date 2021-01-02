b1a137c10b706e7a60c69054c091a9c2577c5c8a
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-01-02 12:53:14 +0530

    auth directive setup in users

--- src/graphql/users/resolvers/index.js
+++ src/graphql/users/resolvers/index.js
@@ -1,9 +1,11 @@
 const authMutations = require("./authMutations");
 const { User, userQueries } = require("./User");
 const { Address, AddressQueries } = require("./Address");
+const { Roles } = require("../authorization");
 const resolvers = {
   User,
   Address,
+  Roles,
   Query: { ...userQueries, ...AddressQueries },
   Mutation: { ...authMutations },
 };
--- src/graphql/users/typedefs/index.js
+++ src/graphql/users/typedefs/index.js
@@ -3,12 +3,14 @@ const entities = require("./entities");
 const enums = require("./enums");
 const mutations = require("./authMutations");
 const queries = require("./queries");
+const { authTypes } = require("../authorization");
 
 const typeDefs = gql`
   ${entities}
   ${queries}
   ${mutations}
   ${enums}
+  ${authTypes}
 `;
 
 module.exports = typeDefs;
